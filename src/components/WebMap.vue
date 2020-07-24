ddd2de8fba79f8e86a08d80ae0480e457c1b5c49
Tom Flitterman <tfl@pers-ee.com>
2020-07-24 16:49:06 +0200

    Display personnalised map

---
+++ src/components/WebMap.vue
@@ -0,0 +1,45 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { loadModules } from 'esri-loader';
+
+export default {
+  name: 'WebMap',
+
+  data() {
+    return {
+      view: null,
+    };
+  },
+
+  mounted() {
+    loadModules(['esri/WebMap', 'esri/views/MapView'], { css: true })
+      .then(([WebMap, MapView]) => {
+        const map = new WebMap({
+          portalItem: {
+            id: 'cc606b220ec0406885924a4297882f6a',
+            portal: 'https://www.arcgis.com',
+          },
+        });
+
+        this.view = new MapView({
+          container: this.$el,
+          map,
+          center: [-118, 34],
+          zoom: 13,
+        });
+      });
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+div {
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>

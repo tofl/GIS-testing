1272016a81d1908b1fd9d5f720eb0c847d6f5998
Tom Flitterman <tfl@pers-ee.com>
2020-07-24 17:47:19 +0200

    Added click event

--- src/components/WebMap.vue
+++ src/components/WebMap.vue
@@ -27,10 +27,32 @@ export default {
         this.view = new MapView({
           container: this.$el,
           map,
-          center: [-118, 34],
-          zoom: 13,
+          center: [2.35, 48.85],
+          zoom: 11,
+        });
+
+        this.view.popup.autoOpenEnabled = false; // Disable the default popup behavior
+
+        this.view.on('click', (event) => { // Listen for the click event
+          this.view.hitTest(event)
+            .then((hitTestResults) => {
+              if (hitTestResults.results) {
+                this.view.popup.open({ // open a popup to show some of the results
+                  location: event.mapPoint,
+                  title: 'Hit Test Results',
+                  content: `${hitTestResults.results.length} + Features Found`,
+                });
+              }
+            });
         });
       });
+
+    setTimeout(() => {
+      this.view.goTo({ center: [5.72, 45.19] }, { duration: 5000 })
+        .then(() => {
+          this.view.zoom = 15;
+        });
+    }, 10000);
   },
 };
 </script>

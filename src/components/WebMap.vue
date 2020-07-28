63d9749c90748ae2e4d2acd3f63033edbda93840
Tom Flitterman <tfl@pers-ee.com>
2020-07-28 18:02:42 +0200

    Added a feature layer

--- src/components/WebMap.vue
+++ src/components/WebMap.vue
@@ -15,44 +15,40 @@ export default {
   },
 
   mounted() {
-    loadModules(['esri/WebMap', 'esri/views/MapView'], { css: true })
-      .then(([WebMap, MapView]) => {
-        const map = new WebMap({
-          portalItem: {
-            id: 'cc606b220ec0406885924a4297882f6a',
-            portal: 'https://www.arcgis.com',
-          },
+    loadModules(
+      ['esri/Map', 'esri/views/MapView', 'esri/Basemap', 'esri/layers/VectorTileLayer', 'esri/layers/FeatureLayer'],
+      { css: true },
+    )
+      .then(([Map, MapView, Basemap, VectorTileLayer, FeatureLayer]) => {
+        const basemap = new Basemap({
+          baseLayers: [
+            new VectorTileLayer({
+              portalItem: {
+                id: '68ebe374c077491c97406a3b2e794471',
+              },
+            }),
+          ],
         });
+
+        const map = new Map({ basemap });
 
         this.view = new MapView({
           container: this.$el,
           map,
-          center: [2.35, 48.85],
-          zoom: 11,
+          center: [4.835659, 45.764043],
+          zoom: 6,
         });
 
-        this.view.popup.autoOpenEnabled = false; // Disable the default popup behavior
-
-        this.view.on('click', (event) => { // Listen for the click event
-          this.view.hitTest(event)
-            .then((hitTestResults) => {
-              if (hitTestResults.results) {
-                this.view.popup.open({ // open a popup to show some of the results
-                  location: event.mapPoint,
-                  title: 'Hit Test Results',
-                  content: `${hitTestResults.results.length} + Features Found`,
-                });
-              }
-            });
+        const stations = new FeatureLayer({
+          url: 'https://services1.arcgis.com/RfENbnrgvatBVii0/arcgis/rest/services/stations/FeatureServer',
+          popupTemplate: {
+            title: '{name}',
+            content: '<b>Location:</b> {address}, {postcode}, {city}<br /><b>Capacity:</b> {capacity}kg/j',
+          },
         });
+
+        map.add(stations);
       });
-
-    setTimeout(() => {
-      this.view.goTo({ center: [5.72, 45.19] }, { duration: 5000 })
-        .then(() => {
-          this.view.zoom = 15;
-        });
-    }, 10000);
   },
 };
 </script>
